7424921e88bd520236b31b27a6c15e14d42e30fb
opranjan <opranjan91700@gmail.com>
2026-05-25 00:34:27 +0530

    update size

--- server.js
+++ server.js
@@ -19,7 +19,14 @@ const app = express();
 app.use(corsAll());
 app.use(helmet());
 app.use(morgan("dev"));
-app.use(express.json());
+// FIX HERE
+app.use(express.json({ limit: "10mb" }));
+
+app.use(express.urlencoded({
+  limit: "10mb",
+  extended: true,
+}));
+
 
 connectDB(MONGO_URI);
 
